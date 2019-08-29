54a494868d2515ee6d05cfc087216433174f2da9
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-08-28 22:05:33 -0600

    rev: Interpreter can STORE_ and LOAD_ variable NAMEs

--- objects.py
+++ objects.py
@@ -3,9 +3,18 @@ class Interpreter:
     def __init__(self):
 
         self.stack = []
+        self.env   = {}
 
     def LOAD_VALUE(self, number):
         self.stack.append(number)
+
+    def STORE_NAME(self, name):
+        val = self.stack.pop()
+        self.env[name] = val
+
+    def LOAD_NAME(self, name):
+        val = self.env[name]
+        self.stack.append(val)
 
     def PRINT_ANSWER(self):
         ans = self.stack.pop()
@@ -17,6 +26,19 @@ class Interpreter:
 
         added = a + b
         self.stack.append(added)
+
+    def parse_arg(self, instr, arg, code_payload):
+        """ Determine what the argument for a given instruction resolves to """
+
+        vals  = ['LOAD_VALUE']
+        names = ['LOAD_NAME'. 'STORE_NAME']
+
+        if   instr in vals:
+            eval_arg = code_payload['vals'][arg]
+        elif instr in names:
+            eval_arg = code_payload['names'][arg]
+
+        return eval_arg
 
     def run(self, code_payload):
 
@@ -35,8 +57,13 @@ class Interpreter:
 
             instr, arg = step
 
+            # wow fantastic job
             if   instr == 'LOAD_VALUE':
                 self.LOAD_VALUE(vals[arg])
+            elif instr == 'STORE_NAME':
+                self.STORE_NAME(arg)
+            elif instr == 'LOAD_NAME':
+                self.LOAD_NAME(arg)
             elif instr == 'ADD_TWO_VALUES':
                 self.ADD_TWO_VALUES()
             elif instr == 'PRINT_ANSWER':
